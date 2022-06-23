b096580ef6c125dd1330360a975114c4b7d144a6
layersony <layersony@gmail.com>
2022-06-23 08:46:49 +0300

    complete assignment

--- src/components/Login.js
+++ src/components/Login.js
@@ -18,10 +18,12 @@ function Login({ setIsLoggedIn }) {
   function handleSubmit(e) {
     e.preventDefault();
 
-    setIsLoggedIn(true);
 
-    // after logging the user in, redirect to the home page!
-    history.push("/");
+    if (formData.username && formData.password){
+      setIsLoggedIn(true);
+      history.push("/");
+
+    }
   }
 
   return (
